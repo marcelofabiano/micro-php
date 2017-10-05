ec5a3dc880e069c2fb761cd567d867a350e28a9b
Marcelo Fabiano <marcelofabiano@hotmail.com.br>
2017-10-05 11:07:29 -0300

    phpunit and autoload

--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
-use App\Router;
-
-$router = new Router();
+$router = new App\Router();
 $router->index();
