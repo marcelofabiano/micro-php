944b7874507141e1c4421735b6a37214524cbcab
Marcelo Fabiano <marcelofabiano@hotmail.com.br>
2017-10-05 13:10:49 -0300

    config whoops

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -6,11 +6,18 @@ require __DIR__.'/../vendor/autoload.php';
 # dotenv autoload
 $basePath = __DIR__ . '/../';
 
+# Var Env
 $dotenv = new \Dotenv\Dotenv($basePath);
 $dotenv->load();
 
+# whoops PHP errors for cool kids
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
 # configs
 $autoloads['config'] = [
+    'basePath' => $basePath,
     'app' => require __DIR__.'/../config/app.php',
     'database' => require __DIR__.'/../config/database.php',
 ];
--- config/app.php
+++ config/app.php
@@ -3,13 +3,17 @@
 # Configurações básicas da aplicação
 $config['app'] = [
     'name'=> getenv('APP_NAME', 'micro-php'),
-    'encoding' => getenv('APP_ENCODING', 'UTF-8')
+    'encoding' => getenv('APP_ENCODING', 'UTF-8'),
+    'error_reporting' => getenv('APP_DEBUG', E_ALL)
 ];
 
-# Diz para o PHP que estamos usando strings UTF-8 até o final do script
+# Encoding para scripts
 mb_internal_encoding($config['app']['encoding']);
 
-# Diz para o PHP que nós vamos enviar uma saída UTF-8 para o navegador
+# Encoding para enviar para saída
 mb_http_output($config['app']['encoding']);
+
+# Nível da saída de erros
+ini_set('error_reporting', $config['app']['error_reporting']);
 
 return $config;
